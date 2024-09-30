c074585acc94487a9231321afd607890affc0825
Bruno Carpio <bcarpio181@gmail.com>
2024-09-30 17:23:37 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,121 @@
+//@ts-check
+
+import jp from "jsonpath";
+
+/**
+ * @param {object} obj
+ * @param {string} key
+ * @param {object} value
+ * @returns {object} a deep copy of the object with the new prop added
+ */
+export function addProp(obj, key, value) {
+    obj = structuredClone(obj);
+    key = "$." + key;
+    let nodes = jp.nodes(obj, key);
+    if (nodes.length === 0) {
+        let star = key.lastIndexOf("*");
+        if (star > 0)
+            key = key.substring(0, star) + 0 + key.substring(star + 1);
+    }
+    jp.value(obj, key, value);
+    return obj;
+}
+
+/**
+ * @param {Array.<object>} objArr an array of objects
+ * @param {string} prop name of the array property to merge
+ * @returns {object} a deep copy of the first object, with the prop array elements concatenated
+ */
+export function mergeObjArr(objArr, prop) {
+    objArr = structuredClone(objArr);
+    let firstObj = objArr.shift();
+    let arrToMerge = jp.query(firstObj, "$." + prop);
+    let to = prop;
+    for (let i = 0; i < objArr.length; i++) {
+        arrToMerge = arrToMerge.concat(jp.query(objArr[i], "$." + to));
+    }
+    if (to.slice(-3) === "[*]") to = to.substring(0, to.length - 3);
+    return addProp(firstObj, to, arrToMerge);
+}
+
+/**
+ * @param {Array.<object>} source an array of objects
+ * @param {Array.<{from: string, to: string}>} mappings an array of transformations to apply to every object
+ * @returns {Array.<object>} an array of objects resulting from transforming the input objects
+ */
+export function mapObjArr(source, mappings) {
+    let result = [];
+    for (let obj of source) {
+        let transformed = mapObj(obj, mappings);
+        for (let obj of transformed) {
+            result.push(obj);
+        }
+    }
+    return result;
+}
+
+/**
+ * @param {object} source input object
+ * @param {Array.<{from: string, to: string}>} mappings an array of transformations to apply to the input object
+ * @returns {Array.<object>} an array of objects resulting from transforming the input object
+ */
+export function mapObj(source, mappings) {
+    let outputObjects = new Map();
+    outputObjects.set(0, [{}]);
+    let arrToMerge = new Set();
+    for (let mapping of mappings) {
+        if (mapping.from) {
+            let to = mapping.to;
+            if (to.includes("[]")) to = to.replaceAll("[]", "[*]");
+            let nodes = jp.nodes(source, "$." + mapping.from);
+            if (nodes.length === 0) continue;
+            if (nodes.length === 1) {
+                for (let v of outputObjects.values()) {
+                    for (let i = 0; i < v.length; i++) {
+                        v[i] = addProp(v[i], to, nodes[0].value);
+                    }
+                }
+            } else {
+                let nodesGroup = Map.groupBy(nodes, (node) => {
+                    for (let el of node.path) {
+                        if (typeof el === "number") return el;
+                    }
+                    return -1;
+                });
+                for (let [k, v] of nodesGroup) {
+                    let arr, obj;
+                    if (outputObjects.get(k)?.length > 0) {
+                        arr = outputObjects.get(k);
+                        obj = arr[0];
+                    } else {
+                        arr = [];
+                        obj = structuredClone(outputObjects.get(0)[0]);
+                    }
+                    for (let i = 0; i < Math.max(v.length, arr.length); i++) {
+                        if (i >= arr.length)
+                            arr.push(addProp(obj, to, v[i].value));
+                        else if (i >= v.length)
+                            arr[i] = addProp(obj, to, v[v.length - 1].value);
+                        else arr[i] = addProp(arr[i], to, v[i].value);
+                    }
+                    outputObjects.set(k, arr);
+                }
+            }
+        }
+        if (mapping.to.includes("[]")) {
+            let to = mapping.to;
+            to = to.replaceAll("[]", "[*]");
+            if (to.slice(-3) !== "[*]") {
+                to = to.substring(0, to.lastIndexOf("[*]") + 3);
+            }
+            arrToMerge.add(to);
+        }
+    }
+    for (let to of arrToMerge.values()) {
+        for (let k of outputObjects.keys()) {
+            outputObjects.set(k, [mergeObjArr(outputObjects.get(k), to)]);
+        }
+    }
+    if (Object.keys(outputObjects.get(0)[0]).length === 0) return [];
+    return Array.from(outputObjects.values()).flat();
+}
---
+++ test/test.js
@@ -0,0 +1,623 @@
+//@ts-check
+
+import assert from "node:assert/strict";
+import { mapObj } from "../index.js";
+import { describe, it } from "node:test";
+
+describe("mapping no nested objects", () => {
+    let transformation = [
+        {
+            from: "key",
+            to: "otherKey",
+        },
+    ];
+
+    it("should return an empty array", () => {
+        let source = {
+            notKey: "some value",
+        };
+        let arr = mapObj(source, transformation);
+        assert.equal(arr.length, 0);
+    });
+
+    it("should map a string value", () => {
+        let source = {
+            key: "some value",
+        };
+        let target = {
+            otherKey: "some value",
+        };
+        let arr = mapObj(source, transformation);
+        assert.equal(arr.length, 1);
+        assert.deepEqual(arr[0], target);
+    });
+
+    it("should map an array value", () => {
+        let source = {
+            key: [1, 2, 3, 4],
+        };
+        let target = {
+            otherKey: [1, 2, 3, 4],
+        };
+        let arr = mapObj(source, transformation);
+        assert.equal(arr.length, 1);
+        assert.deepEqual(arr[0], target);
+    });
+
+    it("should map an object value", () => {
+        let source = {
+            key: {
+                object: "some value",
+            },
+        };
+        let target = {
+            otherKey: {
+                object: "some value",
+            },
+        };
+        let arr = mapObj(source, transformation);
+        assert.equal(arr.length, 1);
+        assert.deepEqual(arr[0], target);
+    });
+
+    it("should map all types with a different name in the target", () => {
+        let transformation = [
+            {
+                from: "number",
+                to: "otherNumber",
+            },
+            {
+                from: "array",
+                to: "otherArray",
+            },
+            {
+                from: "object",
+                to: "otherObject",
+            },
+        ];
+
+        let source = {
+            number: 1,
+            array: [1, 2, 3, 4],
+            object: {
+                object: "value",
+            },
+        };
+
+        let target = {
+            otherNumber: 1,
+            otherArray: [1, 2, 3, 4],
+            otherObject: {
+                object: "value",
+            },
+        };
+
+        let arr = mapObj(source, transformation);
+        assert.equal(arr.length, 1);
+        assert.deepEqual(arr[0], target);
+    });
+});
+
+describe("mapping nested objects", () => {
+    let transformation = [
+        {
+            from: "key",
+            to: "object.anotherKey",
+        },
+    ];
+
+    it("should wrap a string value in the target", () => {
+        let source = {
+            key: "value",
+        };
+
+        let target = {
+            object: {
+                anotherKey: "value",
+            },
+        };
+
+        let arr = mapObj(source, transformation);
+        assert.equal(arr.length, 1);
+        assert.deepEqual(arr[0], target);
+    });
+
+    it("should wrap an array value in the target", () => {
+        let source = {
+            key: [1, 2, 3, 4, 5],
+        };
+
+        let target = {
+            object: {
+                anotherKey: [1, 2, 3, 4, 5],
+            },
+        };
+        let arr = mapObj(source, transformation);
+        assert.equal(arr.length, 1);
+        assert.deepEqual(arr[0], target);
+    });
+
+    it("should wrap an object value in the target", () => {
+        let source = {
+            key: {
+                propOne: 1,
+                propTwo: "two",
+                propThree: [1, 2, 3, "banana"],
+            },
+        };
+
+        let target = {
+            object: {
+                anotherKey: {
+                    propOne: 1,
+                    propTwo: "two",
+                    propThree: [1, 2, 3, "banana"],
+                },
+            },
+        };
+        let arr = mapObj(source, transformation);
+        assert.equal(arr.length, 1);
+        assert.deepEqual(arr[0], target);
+    });
+
+    it("should wrap multiple properties in the target", () => {
+        let transformation = [
+            {
+                from: "object.propOne",
+                to: "objectWrapper.newObject.newPropOne",
+            },
+            {
+                from: "object.propTwo",
+                to: "objectWrapper.newObject.newPropTwo",
+            },
+            {
+                from: "object.propThree",
+                to: "objectWrapper.newObject.newPropThree",
+            },
+        ];
+
+        let source = {
+            object: {
+                propOne: 1,
+                propTwo: "two",
+                propThree: [1, 2, 3, "banana"],
+            },
+        };
+
+        let target = {
+            objectWrapper: {
+                newObject: {
+                    newPropOne: 1,
+                    newPropTwo: "two",
+                    newPropThree: [1, 2, 3, "banana"],
+                },
+            },
+        };
+        let arr = mapObj(source, transformation);
+        assert.equal(arr.length, 1);
+        assert.deepEqual(arr[0], target);
+    });
+});
+
+describe("mapping array values in the source object", () => {
+    let source = {
+        date: "20240921",
+        items: [
+            {
+                item: 11111,
+                availableCountries: [
+                    {
+                        country: "US",
+                    },
+                    {
+                        country: "PE",
+                    },
+                    {
+                        country: "CH",
+                    },
+                ],
+            },
+            {
+                item: 22222,
+                availableCountries: [
+                    {
+                        country: "UY",
+                    },
+                ],
+            },
+            {
+                item: 33333,
+                availableCountries: [
+                    {
+                        country: "BR",
+                    },
+                ],
+            },
+        ],
+    };
+
+    it("should create one object output, no extra property", () => {
+        let transformation = [
+            {
+                from: "items[0].item",
+                to: "item",
+            },
+        ];
+
+        let target = {
+            item: 11111,
+        };
+
+        let arr = mapObj(source, transformation);
+        assert.equal(arr.length, 1);
+        assert.deepEqual(arr[0], target);
+    });
+
+    it("should create one object output, and an extra non array value", () => {
+        let transformation = [
+            {
+                from: "items[0].item",
+                to: "item",
+            },
+            {
+                from: "date",
+                to: "date",
+            },
+        ];
+
+        let target = {
+            date: "20240921",
+            item: 11111,
+        };
+
+        let arr = mapObj(source, transformation);
+        assert.equal(arr.length, 1);
+        assert.deepEqual(arr[0], target);
+    });
+
+    it("should create three objects one of each item", () => {
+        let transformation = [
+            {
+                from: "items[*].item",
+                to: "item",
+            },
+        ];
+
+        let target = [
+            {
+                item: 11111,
+            },
+            {
+                item: 22222,
+            },
+            {
+                item: 33333,
+            },
+        ];
+
+        let arr = mapObj(source, transformation);
+        assert.deepEqual(arr, target);
+    });
+
+    it("should create two objects and an additional non array field", () => {
+        let transformation = [
+            {
+                from: "items[*].item",
+                to: "item",
+            },
+            {
+                from: "date",
+                to: "date",
+            },
+        ];
+
+        let target = [
+            {
+                date: "20240921",
+                item: 11111,
+            },
+            {
+                date: "20240921",
+                item: 22222,
+            },
+            {
+                date: "20240921",
+                item: 33333,
+            },
+        ];
+
+        let arr = mapObj(source, transformation);
+        assert.deepEqual(arr, target);
+    });
+
+    it("should create one object for every country value, nested array", () => {
+        let transformation = [
+            {
+                from: "items[*].availableCountries[*].country",
+                to: "constraints.availableCountry",
+            },
+        ];
+
+        let target = [
+            {
+                constraints: {
+                    availableCountry: "US",
+                },
+            },
+            {
+                constraints: {
+                    availableCountry: "PE",
+                },
+            },
+            {
+                constraints: {
+                    availableCountry: "CH",
+                },
+            },
+            {
+                constraints: {
+                    availableCountry: "UY",
+                },
+            },
+            {
+                constraints: {
+                    availableCountry: "BR",
+                },
+            },
+        ];
+
+        let arr = mapObj(source, transformation);
+        assert.deepEqual(arr, target);
+    });
+
+    it("should create one object for every country value and an item value header for each of the five", () => {
+        let transformation = [
+            {
+                from: "items[*].item",
+                to: "item",
+            },
+            {
+                from: "items[*].availableCountries[*].country",
+                to: "constraints.availableCountry",
+            },
+        ];
+
+        let target = [
+            {
+                item: 11111,
+                constraints: {
+                    availableCountry: "US",
+                },
+            },
+            {
+                item: 11111,
+                constraints: {
+                    availableCountry: "PE",
+                },
+            },
+            {
+                item: 11111,
+                constraints: {
+                    availableCountry: "CH",
+                },
+            },
+            {
+                item: 22222,
+                constraints: {
+                    availableCountry: "UY",
+                },
+            },
+            {
+                item: 33333,
+                constraints: {
+                    availableCountry: "BR",
+                },
+            },
+        ];
+
+        let arr = mapObj(source, transformation);
+        assert.deepEqual(arr, target);
+    });
+
+    it("should create one object for every array value and the non repeating value should be present all of them", () => {
+        let transformation = [
+            {
+                from: "date",
+                to: "date",
+            },
+            {
+                from: "items[*].item",
+                to: "item",
+            },
+            {
+                from: "items[*].availableCountries[*].country",
+                to: "constraints.availableCountry",
+            },
+        ];
+
+        let target = [
+            {
+                date: "20240921",
+                item: 11111,
+                constraints: {
+                    availableCountry: "US",
+                },
+            },
+            {
+                date: "20240921",
+                item: 11111,
+                constraints: {
+                    availableCountry: "PE",
+                },
+            },
+            {
+                date: "20240921",
+                item: 11111,
+                constraints: {
+                    availableCountry: "CH",
+                },
+            },
+            {
+                date: "20240921",
+                item: 22222,
+                constraints: {
+                    availableCountry: "UY",
+                },
+            },
+            {
+                date: "20240921",
+                item: 33333,
+                constraints: {
+                    availableCountry: "BR",
+                },
+            },
+        ];
+
+        let arr = mapObj(source, transformation);
+        assert.deepEqual(arr, target);
+    });
+
+    describe("mapping array values in the target object", () => {
+        let source = {
+            date: "20240921",
+            items: [
+                {
+                    item: 11111,
+                    availableCountries: [
+                        {
+                            country: "US",
+                            countryName: "United States",
+                        },
+                        {
+                            country: "PE",
+                            countryName: "Peru",
+                        },
+                        {
+                            country: "CH",
+                            countryName: "Chile",
+                        },
+                    ],
+                },
+                {
+                    item: 22222,
+                    availableCountries: [
+                        {
+                            country: "UY",
+                            countryName: "Uruguay",
+                        },
+                    ],
+                },
+                {
+                    item: 33333,
+                    availableCountries: [
+                        {
+                            country: "BR",
+                            countryName: "Brasil",
+                        },
+                    ],
+                },
+            ],
+        };
+
+        it("should aggregate all countries for item", () => {
+            let transformation = [
+                {
+                    from: "date",
+                    to: "date",
+                },
+                {
+                    from: "items[*].item",
+                    to: "item",
+                },
+                {
+                    from: "items[*].availableCountries[*].country",
+                    to: "availableCountries[]",
+                },
+            ];
+
+            let target = [
+                {
+                    date: "20240921",
+                    item: 11111,
+                    availableCountries: ["US", "PE", "CH"],
+                },
+                {
+                    date: "20240921",
+                    item: 22222,
+                    availableCountries: ["UY"],
+                },
+                {
+                    date: "20240921",
+                    item: 33333,
+                    availableCountries: ["BR"],
+                },
+            ];
+            let arr = mapObj(source, transformation);
+            assert.deepEqual(arr, target);
+        });
+
+        it("should add properties to objects in the target array", () => {
+            let transformation = [
+                {
+                    from: "date",
+                    to: "date",
+                },
+                {
+                    from: "items[*].item",
+                    to: "item",
+                },
+                {
+                    from: "items[*].availableCountries[*].country",
+                    to: "availableCountries[].code",
+                },
+                {
+                    from: "items[*].availableCountries[*].countryName",
+                    to: "availableCountries[].name",
+                },
+            ];
+
+            let target = [
+                {
+                    date: "20240921",
+                    item: 11111,
+                    availableCountries: [
+                        {
+                            code: "US",
+                            name: "United States",
+                        },
+                        {
+                            code: "PE",
+                            name: "Peru",
+                        },
+                        {
+                            code: "CH",
+                            name: "Chile",
+                        },
+                    ],
+                },
+                {
+                    date: "20240921",
+                    item: 22222,
+                    availableCountries: [
+                        {
+                            code: "UY",
+                            name: "Uruguay",
+                        },
+                    ],
+                },
+                {
+                    date: "20240921",
+                    item: 33333,
+                    availableCountries: [
+                        {
+                            code: "BR",
+                            name: "Brasil",
+                        },
+                    ],
+                },
+            ];
+            let arr = mapObj(source, transformation);
+            assert.deepEqual(arr, target);
+        });
+    });
+});
