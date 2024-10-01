ab2a9fbfc8c261834ec00b8ec496b59e944023aa
Bruno Carpio <bcarpio181@gmail.com>
2024-10-01 12:49:33 -0300

    Add Babel config

---
+++ dist/index.js
@@ -0,0 +1,128 @@
+import "core-js/modules/esnext.map.group-by.js";
+//@ts-check
+
+import jp from "jsonpath";
+
+/**
+ * Add a `key` property to the object `obj` with the value `value`.
+ * @param {object} obj input object.
+ * @param {string} key
+ * @param {object} value
+ * @returns {object} a deep copy of the object with the new property added.
+ */
+export function addProp(obj, key, value) {
+  obj = structuredClone(obj);
+  key = "$." + key;
+  let nodes = jp.nodes(obj, key);
+  if (nodes.length === 0) {
+    let star = key.lastIndexOf("*");
+    if (star > 0) key = key.substring(0, star) + 0 + key.substring(star + 1);
+  }
+  jp.value(obj, key, value);
+  return obj;
+}
+
+/**
+ * Merge the `prop` array values of  the object `objArr`.
+ * @param {Array.<object>} objArr an array of objects.
+ * @param {string} prop key name of the array property to merge.
+ * @returns {object} a deep copy of the first object in the array, with the `prop` array values concatenated.
+ */
+export function mergeObjArr(objArr, prop) {
+  objArr = structuredClone(objArr);
+  let firstObj = objArr.shift();
+  let arrToMerge = jp.query(firstObj, "$." + prop);
+  let to = prop;
+  for (let i = 0; i < objArr.length; i++) {
+    arrToMerge = arrToMerge.concat(jp.query(objArr[i], "$." + to));
+  }
+  if (to.slice(-3) === "[*]") to = to.substring(0, to.length - 3);
+  return addProp(firstObj, to, arrToMerge);
+}
+
+/**
+ * Transforms an input array of objects `source` given the provided array of mappings `mappings`.
+ * @param {Array.<object>} source an array of objects.
+ * @param {Array.<{from: string, to: string}>} mappings an array of transformations to apply to every object.
+ * @returns {Array.<object>} an array of objects resulting from transforming the input objects.
+ */
+export function mapObjArr(source, mappings) {
+  let result = [];
+  for (let obj of source) {
+    let transformed = mapObj(obj, mappings);
+    for (let obj of transformed) {
+      result.push(obj);
+    }
+  }
+  return result;
+}
+
+/**
+ * Transforms an input object `source` given the provided array of mappings `mappings`.
+ * @param {object} source input object.
+ * @param {Array.<{from: string, to: string}>} mappings an array of transformations to apply to the input object.
+ * @returns {Array.<object>} an array of objects resulting from transforming the input object.
+ */
+export function mapObj(source, mappings) {
+  let outputObjects = new Map();
+  outputObjects.set(0, [{}]);
+  let arrToMerge = new Set();
+  for (let mapping of mappings) {
+    if (mapping.from) {
+      let to = mapping.to;
+      if (to.includes("[]")) to = to.replaceAll("[]", "[*]");
+      let nodes = jp.nodes(source, "$." + mapping.from);
+      if (nodes.length === 0) continue;
+      if (nodes.length === 1) {
+        for (let v of outputObjects.values()) {
+          for (let i = 0; i < v.length; i++) {
+            v[i] = addProp(v[i], to, nodes[0].value);
+          }
+        }
+      } else {
+        let nodesGroup = Map.groupBy(nodes, node => {
+          for (let el of node.path) {
+            if (typeof el === "number") return el;
+          }
+          return -1;
+        });
+        for (let [k, v] of nodesGroup) {
+          let arr, obj;
+          if (outputObjects.get(k)?.length > 0) {
+            arr = outputObjects.get(k);
+            obj = arr[0];
+          } else {
+            arr = [];
+            obj = structuredClone(outputObjects.get(0)[0]);
+          }
+          for (let i = 0; i < Math.max(v.length, arr.length); i++) {
+            if (i >= arr.length) arr.push(addProp(obj, to, v[i].value));else if (i >= v.length) arr[i] = addProp(obj, to, v[v.length - 1].value);else arr[i] = addProp(arr[i], to, v[i].value);
+          }
+          outputObjects.set(k, arr);
+        }
+      }
+    }
+    if (mapping.to.includes("[]")) {
+      let to = mapping.to;
+      to = to.replaceAll("[]", "[*]");
+      if (to.slice(-3) !== "[*]") {
+        to = to.substring(0, to.lastIndexOf("[*]") + 3);
+      }
+      arrToMerge.add(to);
+    }
+  }
+  for (let [k, v] of outputObjects) {
+    let filtered = v.filter(obj => Object.keys(obj).length > 0);
+    if (filtered.length === 0) {
+      outputObjects.delete(k);
+      continue;
+    }
+    outputObjects.set(k, filtered);
+  }
+  for (let to of arrToMerge.values()) {
+    for (let k of outputObjects.keys()) {
+      outputObjects.set(k, [mergeObjArr(outputObjects.get(k), to)]);
+    }
+  }
+  return Array.from(outputObjects.values()).flat();
+}
